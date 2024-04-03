c1703ab4453cb98455ff5d2230aef641048e4d6c
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-03 03:15:48 +0300

    SystemUI: Show big keyguard clock when media controls are hidden
    Was hiding even when media notification isn't showing on keyguard.
    Playing media alone shouldn't cause this unexpected behavior
    Preserves old behavior when media is shown on keyguard
    
    Change-Id: I02ff5736bb5d98f768113d739972bf0558790b9a

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1751,7 +1751,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     @ClockSize
     private int computeDesiredClockSizeForSingleShade() {
-        if (hasVisibleNotifications()) {
+        if (hasVisibleNotifications(true)) {
             return SMALL;
         }
         return LARGE;
@@ -1772,7 +1772,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!MigrateClocksToBlueprint.isEnabled()) {
             boolean bypassEnabled = mKeyguardBypassController.getBypassEnabled();
             if (mKeyguardStatusViewController.isLargeClockBlockingNotificationShelf()
-                    && hasVisibleNotifications() && (isOnAod() || bypassEnabled)) {
+                    && hasVisibleNotifications(true) && (isOnAod() || bypassEnabled)) {
                 return SMALL;
             }
         }
@@ -1849,13 +1849,22 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     }
 
     private boolean hasVisibleNotifications() {
+        return hasVisibleNotifications(false);
+    }
+
+    private boolean hasVisibleNotifications(boolean onKeyguard) {
+        final boolean mediaOnKeyguard = !isOnAod()
+                && mMediaHierarchyManager.getShouldShowOnLockScreen();
+        final boolean isMediaVisibleToUser =
+                mMediaDataManager.hasActiveMediaOrRecommendation()
+                && (mediaOnKeyguard || !onKeyguard);
         if (FooterViewRefactor.isEnabled()) {
             return mActiveNotificationsInteractor.getAreAnyNotificationsPresentValue()
-                    || mMediaDataManager.hasActiveMediaOrRecommendation();
+                    || isMediaVisibleToUser;
         } else {
             return mNotificationStackScrollLayoutController
                     .getVisibleNotificationCount() != 0
-                    || mMediaDataManager.hasActiveMediaOrRecommendation();
+                    || isMediaVisibleToUser;
         }
     }
 
